1b361e4335b62fce9dbe5526839f9d0468958a5e
Adriann Paranhos <adriannparanhos@gmail.com>
2026-07-22 13:32:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Dashboard de Vagas</h1>
+      <p>O ambiente web foi restaurado com sucesso. Pronto para iniciar o Kanban.</p>
+    </div>
+  );
+}
+
+export default App;
